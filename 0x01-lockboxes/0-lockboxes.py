ae6bd75c7ebe3af88bf452b84e6cbdc461646caf
2Hasan2 <h01554176846@gmail.com>
2024-08-02 02:48:40 +0300

    2024-08-02 02:48:40 ص

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,21 @@
+#!/usr/bin/python3
+'''A module for determining if all boxes can be unlocked.
+'''
+
 def canUnlockAll(boxes):
+    '''Determines if all boxes can be opened.
+
+    Args:
+        boxes (list of list of int): A list of lists where each inner list contains keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+
+    The function starts with the first box (index 0) which is unlocked by default. It uses Breadth-First Search (BFS)
+    to explore all the boxes that can be unlocked using the keys found in each unlocked box. It tracks the unlocked 
+    boxes in a set and uses a queue to manage the boxes to be processed. After processing, it checks if all boxes are 
+    unlocked by comparing the count of unlocked boxes to the total number of boxes.
+    '''
     # Initialize a set to track unlocked boxes
     unlocked = set()
     # Use a list as a queue for BFS
