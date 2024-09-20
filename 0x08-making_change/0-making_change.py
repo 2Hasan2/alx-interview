5eed989c904d397dc4711c2b01673bf7b043d1c8
2Hasan2 <h01554176846@gmail.com>
2024-09-21 01:07:55 +0300

    2024-09-21 01:07:55 AM

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,8 +4,7 @@
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determine the fewest number of coins needed to
+    """Given a pile of coins of different values, determine the fewest number of coins needed to
     meet a given amount total"""
 
     if total <= 0:
